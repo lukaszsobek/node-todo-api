c72947d280f608736ff24551e2bf5cc223503574
Lukasz Sobek <lukaszsobek@me.com>
2018-08-18 21:56:35 +0200

    Adds first post route

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+app.use(bodyParser.json());
+
 const { mongoose } = require("./db/mongoose");
 const { Todo } = require("./models/todoModel");
 const { User } = require("./models/userModel");
@@ -9,6 +11,20 @@ const { User } = require("./models/userModel");
 
 app.get("/", (req,res) => {
     res.send("Hello");
+});
+
+app.post("/todos", (req,res) => {
+    // res.send(req.body);
+    const todo = new Todo({
+        text: req.body.text
+    });
+    todo.save()
+        .then(doc => {
+            res.send("Document added"); 
+        })
+        .catch(err => {
+            res.status(400).send(err)
+        });
 });
 
 app.listen(3000,() => {
